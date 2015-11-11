832f3ee683f8051a28dde3cddaf090ad6a5c48e0
StevenFen <739833690@qq.com>
2015-11-11 19:54:29 +0800

    创建了一个项目

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by fengjiwen on 15/11/11.
+//  Copyright © 2015年 fengjiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
